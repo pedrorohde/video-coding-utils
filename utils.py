223fc6c069f7ec48518fbdd7426a46134000af3b
pedrorohde <3650280+pedrorohde@users.noreply.github.com>
2020-10-14 11:48:33 -0300

    added YCrCb support

--- utils.py
+++ utils.py
@@ -73,12 +73,73 @@ def random_crops_from_dir(path, n, size):
         path: string with directory path
         n: number of random crops to take
         size: size of the patches (rows, cols)
+    Output:
+        cropped patches as np.array with shape (n, rows, cols, channels)
     """
     filelist = list_files(path, 'png')
     data = np.empty((0, *size, 3))
     for f in filelist:
         img = read_image(f)
         data = np.concatenate((data, n_random_crops(img, n, size)),axis=0)
+    return data
+
+
+def read_YUV420(path, size, frame=0, channel='Y'):
+    """
+    Read binary YUV420 raw video file and return one frame.
+
+    Input:
+        path: string with file path
+        size: size of each frame (Y)
+        frame: which frame to take
+        channel: which channel to take
+    Output:
+        frame as np.array, normalized
+    """
+    pos = frame*size[0]*size[1]*6//4
+    if channel in ['U', 'u', 'Cb', 'cb']:
+        pos += size[0]*size[1]
+        size = (size[0]//2, size[1]//2)
+    elif channel in ['V', 'v', 'Cr', 'cr']:
+        pos += size[0]*size[1]*5//4
+        size = (size[0]//2, size[1]//2)
+    
+    with open(filepath, 'rb') as f:
+        f.seek(pos, 0)
+        img = Image.frombytes('L', [size[1], size[0]], f.read(size[1]*size[0]))
+    
+    return np.asarray(img)/255.
+
+
+def read_YUV420_multiframes(filepath, size, frames, channel='Y'):
+    """
+    Read binary YUV420 raw video file and return multiple frames.
+
+    Input:
+        path: string with file path
+        size: size of each frame (Y)
+        frame: list of frames to take
+        channel: which channel to take
+    Output:
+        frames as np.array with shape (n, rows, cols), normalized
+    """
+    pos = [frame*size[0]*size[1]*6//4 for frame in frames]
+    if channel in ['U', 'u', 'Cb', 'cb']:
+        pos = [p + size[0]*size[1] for p in pos]
+        size = (size[0]//2, size[1]//2)
+    elif channel in ['V', 'v', 'Cr', 'cr']:
+        pos = [p + size[0]*size[1]*5//4 for p in pos]
+        size = (size[0]//2, size[1]//2)
+    
+    data = np.empty((len(frames), *size))
+    
+    with open(filepath, 'rb') as f:
+        for i in range(len(pos)):
+            p = pos[i]
+            f.seek(p, 0)
+            img = Image.frombytes('L', [size[1], size[0]], f.read(size[1]*size[0]))
+            data[i,:,:] = np.asarray(img)/255.
+             
     return data
 
 
