e0d4278fc0651e6526fa6426707d71e4d2346f97
pedrorohde <3650280+pedrorohde@users.noreply.github.com>
2020-10-21 17:44:00 -0300

    H.264 encoding/decoding shell script

---
+++ YUV_H264_YUV.sh
@@ -0,0 +1,16 @@
+#sudo snap install ffmpeg
+
+#WIDTH=
+#HEIGHT=
+#FPS=
+#QP=
+#INPUT_FILE=
+#ENCODED_FILE=
+#DECODED_FILE=
+
+ffmpeg -f rawvideo -vcodec rawvideo -s $WIDTH\x$HEIGHT -r $FPS -pix_fmt yuv420p -i $INPUT_FILE -c:v libx264 -qp $QP $ENCODED_FILE
+
+ffmpeg -i $ENCODED_FILE -vcodec rawvideo -pix_fmt yuv420p $DECODED_FILE
+
+
+
